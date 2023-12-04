3dd716747b6eabfb89d096955c8ca9d7fb6db64a
Carvalj019 <57874906+Carvalj019@users.noreply.github.com>
2023-12-03 23:34:45 -0500

    Update RemoteTakeoverEX.py

--- RemoteTakeoverEX.py
+++ RemoteTakeoverEX.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     # assuming the first listed device is the one you want to remotely control, use index 0
     ip_address_1 = devices[0]['ip'] #stores ip address of bulb 1
 
-    #make additional variables for each characteristic you'd like to keep track of
+    #make additional variables for each characteristic you'd like to keep track of and access
     #port_1 = devices[0]['port'] #stores port number of bulb 1
 
     #Once you have a bulb's IP Address, you can make commands from a python script
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     bulb_1.turn_on()
     bulb_1.effect = "sudden"
 
-    #This is a simple subscript that will generate a strobe light effect
+    #This is a simple subscript that will generate a strobe light effect for 30 seconds
     time_end = time.time() + 60 * 0.5 #time_end is set 30 seconds after current time
     while time.time() < time_end:
         bulb_1.set_rgb(0, 0, 255)
         bulb_1.set_rgb(255, 255, 0)
 
-    #We are able to remotely control these bulbs due to a lack of encryption and secure connection protocols.
+    #We are able to remotely control these bulbs due to a lack of encryption and secure connection protocols as a design flaw made by the manufacturer.
